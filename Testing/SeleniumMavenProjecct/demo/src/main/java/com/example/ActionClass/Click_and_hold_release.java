3f214f392136d334423073323fa00d71fb042119
vaibhav <vaibhavdeshmukh0703@gmail.com>
2022-07-23 20:56:44 +0530

    Click and Hold and Realease on specifice location

--- Testing/SeleniumMavenProjecct/demo/src/main/java/com/example/ActionClass/Click_and_hold_release.java
+++ Testing/SeleniumMavenProjecct/demo/src/main/java/com/example/ActionClass/Click_and_hold_release.java
@@ -48,6 +48,10 @@ public class Click_and_hold_release {
             System.out.println(getX_Button_A+" ---- "+getY_Button_A);
             System.out.println(getX_ButtonC+"------"+getY_ButtonC);
             Actions actions =  new Actions(driver);
+
+            actions.moveToElement(Button_A).clickAndHold().moveToElement(Button_C).release().build().perform();
+            
+            
            // actions.clickAndHold(Button_A).moveByOffset(218,0).perform();
           //  actions.clickAndHold(Button_A).moveByOffset(getX+1,getY+1).perform();
           //  actions.dragAndDrop(Button_A, Button_C).build().perform();
